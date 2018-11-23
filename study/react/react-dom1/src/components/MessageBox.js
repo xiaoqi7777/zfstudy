c82ee11872a556be190cb4ffaacdd7b4d8c9df77
xiaoqi7777 <507198525@qq.com>
2018-11-23 08:36:58 +0800

    1

---
+++ study/react/react-dom1/src/components/MessageBox.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class MessageBox extends Component {
+  render() {
+    return (
+      <div>
+        13
+      </div>
+    )
+  }
+}
+
+export default MessageBox
